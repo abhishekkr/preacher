4cc9713c0ba2f7295bf6ed46ff9abc7eda60994b
AbhishekKr <abhikumar163@gmail.com>
2017-05-11 18:46:15 +0530

    [hn] newStoryIDs, topStoryIDs, bestStoryIDs list get func
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- hackernews/query.go
+++ hackernews/query.go
@@ -8,6 +8,14 @@ import (
 	"github.com/abhishekkr/gol/golhttpclient"
 )
 
+var (
+	itemURI = "https://hacker-news.firebaseio.com/v0/item/%d.json"
+
+	newStoriesURI  = "https://hacker-news.firebaseio.com/v0/newstories"
+	topStoriesURI  = "https://hacker-news.firebaseio.com/v0/topstories"
+	bestStoriesURI = "https://hacker-news.firebaseio.com/v0/beststories"
+)
+
 type DataItem struct {
 	By          string `json:"by"`
 	Descendants int    `json:"descendants"` //only-story
@@ -22,13 +30,49 @@ type DataItem struct {
 }
 
 func (item *DataItem) Get(itemID int) {
-	uri := fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%d.json", itemID)
+	uri := fmt.Sprintf(itemURI, itemID)
 	responseBody, err := golhttpclient.HttpGet(uri, map[string]string{}, map[string]string{})
 	if err != nil {
 		log.Printf("[error] %s", err.Error())
 	}
 
 	if err = json.Unmarshal([]byte(responseBody), &item); err != nil {
+		log.Printf("[error] %s", err.Error())
+	}
+	return
+}
+
+func NewStoryIDs(itemID int) (itemIDs []int) {
+	responseBody, err := golhttpclient.HttpGet(newStoriesURI, map[string]string{}, map[string]string{})
+	if err != nil {
+		log.Printf("[error] %s", err.Error())
+	}
+
+	if err = json.Unmarshal([]byte(responseBody), &itemIDs); err != nil {
+		log.Printf("[error] %s", err.Error())
+	}
+	return
+}
+
+func TopStoryIDs(itemID int) (itemIDs []int) {
+	responseBody, err := golhttpclient.HttpGet(topStoriesURI, map[string]string{}, map[string]string{})
+	if err != nil {
+		log.Printf("[error] %s", err.Error())
+	}
+
+	if err = json.Unmarshal([]byte(responseBody), &itemIDs); err != nil {
+		log.Printf("[error] %s", err.Error())
+	}
+	return
+}
+
+func BestStoryIDs(itemID int) (itemIDs []int) {
+	responseBody, err := golhttpclient.HttpGet(bestStoriesURI, map[string]string{}, map[string]string{})
+	if err != nil {
+		log.Printf("[error] %s", err.Error())
+	}
+
+	if err = json.Unmarshal([]byte(responseBody), &itemIDs); err != nil {
 		log.Printf("[error] %s", err.Error())
 	}
 	return
