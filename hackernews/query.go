83c5e46a3c4b97263d953028f268ab5ef27dcfbf
AbhishekKr <abhikumar163@gmail.com>
2017-05-11 16:45:00 +0530

    [hackernews] initial piece of item fetch
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

---
+++ hackernews/query.go
@@ -0,0 +1,49 @@
+package hackernews
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/abhishekkr/gol/golhttpclient"
+)
+
+type DataItem struct {
+	By          string `json:"by"`
+	Descendants int    `json:"descendants"` //only-story
+	ID          int    `json:"id"`
+	Parent      int    `json:"parent"` //only-comment
+	Text        string `json:"text"`   //only-comment
+	Score       int    `json:"score"`  //only-story
+	Time        int    `json:"time"`
+	Title       string `json:"title"` //only-story
+	Type        string `json:"type"`
+	URL         string `json:"url"` //only-story
+}
+
+func (item *DataItem) Get(itemID int) {
+	uri := fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%d.json", itemID)
+	responseBody, err := golhttpclient.HttpGet()
+	if err != nil {
+		log.Printf("[error] %s", err.Error())
+	}
+
+	if err = json.Unmarshal([]byte(responseBody), &item); err != nil {
+		log.Printf("[error] %s", err.Error())
+	}
+	return
+}
+
+func (item DataItem) IsComment() bool {
+	if item.Type == "comment" {
+		return true
+	}
+	return false
+}
+
+func (item DataItem) IsStory() bool {
+	if item.Type == "story" {
+		return true
+	}
+	return false
+}
